23ddf350cfb60a754b0009f2687f93a3ff775570
krumeluren <9928liqv@gmail.com>
2025-12-18 16:41:29 +0100

    Models

---
+++ QuizApp/MongoModels/Answer.cs
@@ -0,0 +1,7 @@
+﻿using MongoDB.Bson;
+
+public class Answer {
+    public string Id { get; set; } = ObjectId.GenerateNewId().ToString();
+    public string Text { get; set; }
+    public bool IsCorrect { get; set; }
+}
--- QuizApp/MongoModels/Question.cs
+++ QuizApp/MongoModels/Question.cs
@@ -1,16 +1,15 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 
-
 [BsonIgnoreExtraElements]
-public class Quiz {
+public class Question {
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
     public string Id { get; set; } = ObjectId.GenerateNewId().ToString();
-    public string Title { get; set; }
+
+    public string Text { get; set; }
 
     public List<string> Tags { get; set; } = new List<string>();
 
-    [BsonRepresentation(BsonType.ObjectId)]
-    public List<string> QuestionIds { get; set; } = new List<string>();
+    public List<Answer> Answers { get; set; } = new List<Answer>();
 }
